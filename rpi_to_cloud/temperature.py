83b1a1e42244f90e800423082b878fdbcb5810a2
Picar <pierre.carette@gmail.com>
2016-12-12 14:22:21 -0800

    Fix datetime and formattting

--- rpi_to_cloud/temperature.py
+++ rpi_to_cloud/temperature.py
@@ -1,12 +1,14 @@
 from Adafruit_BMP.BMP085 import BMP085
+from elasticsearch import Elasticsearch
+from datetime import datetime
+
 sensor = BMP085().read_temperature()
 
-From elasticsearch import Elasticsearch
-Es = Elasticsearch( ['<ip>'])  # AWS ELK Server IP:   35.163.69.146
-Doc = {
-'author': ’<your_name>',
-'temp': temp,
-'timestamp': datetime.now()
+es = Elasticsearch( ['<ip>'])  # AWS ELK Server IP:   35.163.69.146
+doc = {
+    'author': ’<your_name>',
+    'temp': temp,
+    'timestamp': datetime.now()
 }
  
 print es.index(index="iot", doc_type='bmp180', body=doc)
